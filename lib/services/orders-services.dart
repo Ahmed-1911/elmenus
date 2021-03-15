289a759b8d82e801cd1cf3fa38d3cad74b68adcf
mohamed samir <m.samir.ahmed24@gmail.com>
2021-03-15 12:07:22 +0200

    adding post fucnction for order-service

--- lib/services/orders-services.dart
+++ lib/services/orders-services.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:elmenus/model/orders-model.dart';
 import 'package:http/http.dart' as http;
 
-class OrdersApi{
+class OrdersApi {
   Future<List<OrderElement>> fetchOrdersList() async {
     try {
       var url = 'https://db-json-project.herokuapp.com/orders';
@@ -11,18 +11,41 @@ class OrdersApi{
       print(extractedData.toString());
       if (response.statusCode == 200) {
         ListOfOrders ordersList = ListOfOrders.fromJson(extractedData);
-        List<OrderElement> oList=ordersList.orders.map((e) => OrderElement.fromJson(e)).toList();
+        List<OrderElement> oList =
+            ordersList.orders.map((e) => OrderElement.fromJson(e)).toList();
         return oList;
-      }
-      else {
+      } else {
         print(response.statusCode.toString());
       }
       return [];
-    }
-    catch (e) {
+    } catch (e) {
       print(e.toString());
       return null;
     }
   }
 
+  Future<OrderElement> sendOrder(OrderElement order) async {
+    try {
+      var url = 'https://db-json-project.herokuapp.com/orders';
+      final response = await http.post(
+        url,
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, String>{
+          'resturant': order.restaurant,
+          'user': order.user,
+          'totalprice': order.totalPrice,
+          'deliveryfee': order.deliveryFee
+        }),
+      );
+      if (response.statusCode == 200) {
+        return OrderElement.fromJson(jsonDecode(response.body));
+      } else {
+        throw Exception('Failed to send data');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
 }
